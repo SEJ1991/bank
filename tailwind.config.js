4a91081f40dce8b548caf2731a7f612d991e9b47
SEJ <jink6111@gmail.com>
2024-11-03 19:00:06 +0900

    fix: [B-1] fix color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      primary: {
-        DEFAULT: '#202020',
-        300: '#2E2E2E',
-        500: '#1E1E1E',
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#202020',
+          300: '#2E2E2E',
+          500: '#1E1E1E',
+        },
       },
     },
-    extend: {},
   },
   plugins: [],
 };
