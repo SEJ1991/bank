24bc93bccebb3feac06cd65bc9bec4a50c5a6326
SEJ <jink6111@gmail.com>
2024-11-04 21:05:46 +0900

    feat: [B-1] add onClick props in ContentsFrame

--- src/components/frame/ContentsFrame.tsx
+++ src/components/frame/ContentsFrame.tsx
@@ -3,9 +3,12 @@ import { twMerge } from 'tailwind-merge';
 interface Props {
   className?: string;
   children?: React.ReactNode;
+  onClick?: () => void;
 }
-export default function ContentsFrame({ className, children }: Props) {
+export default function ContentsFrame({ className, children, onClick }: Props) {
   return (
-    <section className={twMerge('bg-primary-300 p-2 rounded-lg', className)}>{children}</section>
+    <section className={twMerge('bg-primary-300 p-4 rounded-lg', className)} onClick={onClick}>
+      {children}
+    </section>
   );
 }
